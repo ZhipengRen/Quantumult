f7014e92215c6eeb1b6a4704f41e784b1e9b0550
ZhipengRen <renzp08@163.com>
2026-01-17 16:51:16 +0800

    Log request URL and bundle identifier
    Add console logs for request URL and bundle identifier.

--- ipa/ipa-install.js
+++ ipa/ipa-install.js
@@ -3,10 +3,12 @@
  */
 
 let requestURL = $request.url;
+console.log(requestURL);
 
 //let re = /plist\/(.*)\.plist$/;
 let re = /bundleIdentifier=(.*)/;
 let bundleIdentifier = re.exec(requestURL)[1]
+console.log(bundleIdentifier);
 
 let html = `
 	<?xml version="1.0" encoding="UTF-8"?>
